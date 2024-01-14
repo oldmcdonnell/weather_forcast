f599d09a58b95910843ec10429fbd5c40491d27f
oldmc <oldmcdnnell@gmail.com>
2024-01-13 23:24:21 -0500

    updated main temperature code, added plot, moved code from backend to main
    updated backend

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+#Add title, text input, slider, select box, and subheader
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -10,10 +11,18 @@ option = st.selectbox("Select data to view",
                        ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(placem days, option)
+#Get temperature or dky data
+filtered_data = get_data(place, days)
 
-d, t = get_data(days)
+#create a temperatur plot
+if option == "Temperature":
+    temperatures = [dict['main']['temp'] for dict in filtered_data]
+    dates = [dict["dt_txt"] for dict in filtered_data]
+    figure = px.line(x=dates, y=temperatures, labels={"x": "Date",
+                                   "y": "Temperature (C)"})
+    st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x": "Date",
-                               "y": "Temperature (C)"})
-st.plotly_chart(figure)
+#clouds
+if option == "Sky":
+    filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    st.image()
